0740e4ebdf26b5da5e7ffcb104912ae9f09e4886
Jose Soler <jose.soler@zanox.com>
2015-04-14 21:46:27 +0200

    Add random behaviour

--- src/main/java/com/java/learning/model/Filesystem.java
+++ src/main/java/com/java/learning/model/Filesystem.java
@@ -3,16 +3,23 @@ package com.java.learning.model;
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by jsoler on 14.04.15.
  */
 public class Filesystem implements Closeable {
 
+    Random generator = new Random();
+
+
     public void close() throws IOException {
         System.out.println("Closing resource");
     }
 
     public void backupItems(List<BillableItem> processedItems) {
+        if (generator.nextBoolean()) {
+            throw new RuntimeException("Filesystem Error");
+        }
     }
 }
--- src/main/java/com/java/learning/model/Network.java
+++ src/main/java/com/java/learning/model/Network.java
@@ -2,14 +2,23 @@ package com.java.learning.model;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.rmi.UnexpectedException;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by jsoler on 14.04.15.
  */
 public class Network implements Closeable {
 
+    Random generator = new Random();
+
     public void notifyExternalSystems(List<BillableItem> processedItems) {
+
+        if (generator.nextBoolean()) {
+           throw new RuntimeException("Network Error");
+        }
 
     }
 
